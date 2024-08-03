8f4b3ddb0888b4b1f8bcbb177887d49d0e4a8508
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-08-03 15:08:47 +0100

    Add app struct

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,33 @@ use std::{
 };
 use style::palette::tailwind;
 
+struct App {
+    current_path: String,
+    elements_list: Vec<String>,
+    selected_elements_list: Vec<String>,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            current_path: String::from("./"),
+            elements_list: Vec::new(),
+            selected_elements_list: Vec::new(),
+        }
+    }
+
+    fn update_path(&mut self, path: String) {
+        self.current_path = path.clone();
+        let paths = fs::read_dir(path).unwrap();
+
+        let str_paths = paths
+            .map(|entry| entry.unwrap().path().to_str().unwrap().to_string())
+            .collect::<Vec<String>>();
+        self.elements_list = str_paths;
+        self.selected_elements_list = Vec::new();
+    }
+}
+
 fn obtain_filenames_table<'a>() -> io::Result<Option<Table<'a>>> {
     let paths = fs::read_dir("./").unwrap();
 
