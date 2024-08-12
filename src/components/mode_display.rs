dc5f26c76bc96dbf88f612aedecbd6d1334022c3
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-08-12 20:44:55 +0100

    wip

---
+++ src/components/mode_display.rs
@@ -0,0 +1,28 @@
+use color_eyre::eyre::Result;
+use ratatui::{layout::Rect, Frame};
+
+use crate::mode::Mode;
+
+use super::Component;
+
+struct ModeDisplay {
+    mode: Mode,
+}
+impl ModeDisplay {
+    pub fn new() -> Self {
+        Self { mode: Mode::Normal }
+    }
+
+    pub fn switch_mode(&mut self, mode: Mode) {
+        self.mode = mode;
+    }
+}
+impl Component for ModeDisplay {
+    fn draw(&mut self, frame: &mut Frame, area: Rect) -> color_eyre::eyre::Result<()> {
+        todo!()
+    }
+
+    fn get_area(&mut self, frame: &mut Frame) -> Result<Option<ratatui::prelude::Rect>> {
+        todo!()
+    }
+}
