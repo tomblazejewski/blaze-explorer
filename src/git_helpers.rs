f45d03ec33ee371c5ca07538bfd181de702fa22d
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-12-25 21:00:04 +0000

    Add more git colours

--- src/git_helpers.rs
+++ src/git_helpers.rs
@@ -14,9 +14,11 @@ pub fn get_repo(path: PathBuf) -> Option<Repository> {
 
 pub fn assign_git_styling(style: Style, status: Status) -> Style {
     match status {
-        Status::WT_MODIFIED => style.fg(Color::Rgb(255, 165, 0)),
+        Status::WT_MODIFIED => style.fg(Color::Rgb(255, 215, 0)),
+        Status::INDEX_MODIFIED => style.fg(Color::Rgb(255, 140, 0)),
         Status::IGNORED => style.fg(Color::Rgb(128, 128, 128)),
-        Status::WT_NEW => style.fg(Color::Rgb(0, 128, 0)),
-        _ => style.fg(Color::Rgb(255, 192, 203)),
+        Status::WT_NEW => style.fg(Color::Rgb(152, 251, 152)),
+        Status::INDEX_NEW => style.fg(Color::Rgb(0, 100, 0)),
+        _ => style.fg(Color::Rgb(255, 20, 147)),
     }
 }
