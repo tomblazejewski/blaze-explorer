f599d000ecd9a6a371489cbf5a9495e3f6341ca8
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-12-26 18:46:09 +0000

    Explorer helpers tests

--- src/explorer_helpers.rs
+++ src/explorer_helpers.rs
@@ -38,3 +38,37 @@ pub fn jump_highlight(
         Line::from(line_text)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_jump_highlight() {
+        let line_text = String::from("Hello world");
+        let query = "w";
+        let char = 'w';
+        let query_style = Style::new().fg(ratatui::style::Color::Red);
+        let char_style = Style::new().fg(ratatui::style::Color::Green);
+        let line = jump_highlight(line_text, query, char, query_style, char_style);
+        let beginning = Span::from("Hello ");
+        let query_span = Span::styled(query, query_style);
+        let char_span = Span::styled(char.to_string(), char_style);
+        let remainder_span = Span::from("rld");
+        let expected_line = Line::from(vec![beginning, query_span, char_span, remainder_span]);
+        assert_eq!(line, expected_line);
+    }
+
+    #[test]
+    fn test_highlight_search_result() {
+        let line_text = String::from("Hello world");
+        let query = "worl";
+        let highlighted_style = Style::new().fg(ratatui::style::Color::Green);
+        let line = highlight_search_result(line_text, query, highlighted_style);
+        let beginning = Span::from("Hello ");
+        let query_span = Span::styled(query, highlighted_style);
+        let ending = Span::from("d");
+        let expected_line = Line::from(vec![beginning, query_span, ending]);
+        assert_eq!(line, expected_line);
+    }
+}
